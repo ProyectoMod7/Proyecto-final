df40f5e997bbe57a43a52474bf9fa625b6ae6353
modulop435 <modulop435@gmail.com>
2025-09-27 10:12:23 -0300

    correoccion models/_init_.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-def init_app(app):
-    db.init_app(app)
-    app.db = db # Attach db to app for easy access
-    with app.app_context():
-        db.create_all()  # Create tables for all models 
-        print("Database tables created.")
-        db.session.commit()
-    return db
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
 
 # Importar los modelos para que SQLAlchemy los registre
 from .area import Area
@@ -23,8 +12,5 @@ from .pieza_instalada import PiezaInstalada
 from .proveedor import Proveedor
 from .stock import Stock
 
-# Add any additional models here
-# For example:
-# from .new_model import NewModel
-# from .another_model import AnotherModel
-# etc.
+# No usamos create_all() porque ya tenemos las tablas en Supabase
+# SQLAlchemy solo va a mapear los modelos a las tablas existentes
