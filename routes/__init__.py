72480119827c7aebb1d237f6bec83473cfa4e27c
modulop435 <modulop435@gmail.com>
2025-09-27 15:32:47 -0300

    casi casi

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,13 @@
+
+from .auth_routes import auth_bp
 from .maquinas_routes import maquinas_bp
 from .piezas_routes import piezas_bp
 from .stock_routes import stock_bp
-from .misc_routes import misc_bp
+from .contacto_routes import contacto_bp
 
 def register_blueprints(app):
+    app.register_blueprint(auth_bp)
     app.register_blueprint(maquinas_bp)
     app.register_blueprint(piezas_bp)
     app.register_blueprint(stock_bp)
-    app.register_blueprint(misc_bp)
+    app.register_blueprint(contacto_bp)
