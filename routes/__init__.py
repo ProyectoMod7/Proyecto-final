d48d507d0a86ceb3d5455f0690413cd5fedb382b
modulop435 <modulop435@gmail.com>
2025-12-09 03:52:19 -0300

    9/12/25

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,6 +3,11 @@ from .maquinas_routes import maquinas_bp
 from .piezas_routes import piezas_bp
 from .stock_routes import stock_bp
 from .contacto_routes import contacto_bp
+from .piezas_instaladas_routes import piezas_instaladas_bp   # 👈 IMPORTAMOS EL BLUEPRINT
+
+# y cuando vayas agregando más:
+# from .fabricantes_routes import fabricantes_bp
+# from .materiales_routes import materiales_bp
 
 def register_blueprints(app):
     app.register_blueprint(auth_bp)
@@ -10,3 +15,7 @@ def register_blueprints(app):
     app.register_blueprint(piezas_bp)
     app.register_blueprint(stock_bp)
     app.register_blueprint(contacto_bp)
+    app.register_blueprint(piezas_instaladas_bp)  # 👈 LO REGISTRAMOS
+
+    # app.register_blueprint(fabricantes_bp)
+    # app.register_blueprint(materiales_bp)
